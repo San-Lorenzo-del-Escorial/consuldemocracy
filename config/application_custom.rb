112419af4265663b4a19bcafd8b03fb56af64af8
Diego Calvo <diego.calvo@enreda.coop>
2024-11-26 14:42:05 +0100

    only spanish locale

--- config/application_custom.rb
+++ config/application_custom.rb
@@ -1,4 +1,9 @@
 module Consul
   class Application < Rails::Application
+    config.i18n.default_locale = :es
+    available_locales = [
+      "es"
+    ]
+    config.i18n.available_locales = available_locales
   end
 end
