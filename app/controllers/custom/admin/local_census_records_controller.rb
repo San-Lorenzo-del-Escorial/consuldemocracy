e5bbdfb9d755e614eb79bb690241666e279acbe6
Diego Calvo <diego.calvo@enreda.coop>
2024-12-20 11:27:41 +0100

    add date of birth to verification

--- app/controllers/custom/admin/local_census_records_controller.rb
+++
@@ -1,8 +0,0 @@
-load Rails.root.join("app", "controllers", "admin", "local_census_records_controller.rb")
-
-class Admin::LocalCensusRecordsController
-  private
-    def allowed_params
-      [:document_type, :document_number, :postal_code]
-    end
-end
