984b2f7edac31f546c4f54e0fc1a488fe597135d
Diego Calvo <diego.calvo@enreda.coop>
2025-05-13 09:56:15 +0200

    only spanish

--- config/application_custom.rb
+++ config/application_custom.rb
@@ -3,7 +3,6 @@ module Consul
     config.i18n.default_locale = :es
     available_locales = [
       "es",
-      "en"
     ]
     config.i18n.available_locales = available_locales
     config.i18n.fallbacks = [I18n.default_locale, {
